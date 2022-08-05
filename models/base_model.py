df417bf75de25bf84cd64847dcb7ce1fe5998613
Bryan Somto <bryansomto@gmail.com>
2022-08-05 05:07:15 +0100

    modified base model

--- models/base_model.py
+++ models/base_model.py
@@ -42,8 +42,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            # models.storage.new(self)
-            # models.storage.save()
+            models.new(self)
+            models.save()
 
     def __str__(self):
         return ('[{}] ({}) {}'.format(
@@ -52,7 +52,7 @@ class BaseModel:
     def save(self):
         """ Updates the public instance attribute 'updated_at'. """
         self.updated_at = datetime.datetime.now()
-        # models.storage.save()
+        models.save()
 
     def to_dict(self):
         """ Returns a dictionary with all keys/values of __dict__ of the instance. """
