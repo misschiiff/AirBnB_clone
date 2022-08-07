0c963e02297507cbcf9ffb595e82ef4e6e33b413
Bryan Somto <bryansomto@gmail.com>
2022-08-07 14:18:21 +0100

    modified file storage engine

--- models/base_model.py
+++ models/base_model.py
@@ -28,22 +28,20 @@ class BaseModel:
             *args (ints): New attribute values.
             **kwargs (dict): New key/value pairs of attributes.
         """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            if hasattr(self, "created_at") and type(self.created_at) is str:
-                self.created_at = datetime.strptime(
-                    kwargs["created_at"], __time).isoformat()
-            if hasattr(self, "updated_at") and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(
-                    kwargs["updated_at"], __time).isoformat()
-        else:
+        if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            # models.new(self)
-            # models.save()
+            models.storage.new(self)
+            models.storage.save()
+        else:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                     __time)
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                     __time)
+            for key, val in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, val)
 
     def __str__(self):
         """ String representation """
@@ -53,7 +51,7 @@ class BaseModel:
     def save(self):
         """ Updates the public instance attribute 'updated_at'. """
         self.updated_at = datetime.now()
-        # models.save()
+        models.storage.save()
 
     def to_dict(self):
         """ Returns a dictionary with all keys/values of __dict__ of the instance. """
@@ -64,27 +62,3 @@ class BaseModel:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(__time)
         new_dict["__class__"] = self.__class__.__name__
         return new_dict
-
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        """ Convert python object to json string. """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
-        return json.dumps(list_dictionaries)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """ Save json string to file """
-        with open(cls.__name__ + ".json", mode="w") as j_file:
-            if list_objs is not None:
-                list_dict = [item.to_dictionary() for item in list_objs]
-                j_file.write(cls.to_json_string(list_dict))
-            else:
-                j_file.write(cls.to_json_string([]))
-
-    @staticmethod
-    def from_json_string(json_string):
-        """ Convert json string to python object. """
-        if json_string is None:
-            return []
-        return json.loads(json_string)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-""" File storage engine. """
+"""
+File storage engine.
+"""
 import json
+import os.path
 
 
 class FileStorage:
@@ -11,11 +14,32 @@ class FileStorage:
         __file_path (str): path to the JSON file.
         __objects (dict): empty but will store all objects by <class name>.id
     """
-    __file_path = 'filestorage.json'
+    __file_path = 'file.json'
     __objects = dict()
 
     def all(self):
+        """ returns the dictionary __objects """
         return self.__objects
 
     def new(self, obj):
-        self.__objects[__class__.__name__].id
+        """
+        sets in __objects the obj with key <obj class name>.id
+        """
+        dict_key = obj.__class__.__name__ + '.' + obj.id
+        self.__objects.update({dict_key: obj})
+
+    def save(self):
+        """ serializes __objects to the JSON file """
+        dict = {}
+        for key in self.__objects:
+            dict[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, "w") as f:
+            json.dump(dict, f)
+
+    def reload(self):
+        """ deserializes the JSON file to __objects """
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, "r") as f:
+                json_obj = json.load(f)
+            for key, val in json_obj.items():
+                self.__objects[key] = eval(val["__class__"])(**val)
