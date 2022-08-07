d7dcfda038a1395a12e03f4ae3d0b9bd78a1cf3f
Bryan Somto <bryansomto@gmail.com>
2022-08-07 14:36:13 +0100

    modified basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,66 +1,51 @@
 #!/usr/bin/python3
-"""
-Module base
-"""
-import json
-import uuid
-from datetime import datetime
+"""Defines the BaseModel class."""
 import models
-# import csv
-# import turtle
-# import random
+from uuid import uuid4
+from datetime import datetime
 
 
 class BaseModel:
-    """
-    Contains Class BaseModel
-
-    Private Class Attributes:
-        __time (str): time format.
-    """
-    global __time
-    __time = "%Y-%m-%dT%H:%M:%S.%f"
+    """Represents the BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new base method.
+        """Initialize a new BaseModel.
 
         Args:
-            *args (ints): New attribute values.
-            **kwargs (dict): New key/value pairs of attributes.
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
         """
-        if len(kwargs) == 0:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            models.storage.new(self)
-            models.storage.save()
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, time_format)
+                else:
+                    self.__dict__[key] = value
         else:
-            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
-                                                     __time)
-            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
-                                                     __time)
-            for key, val in kwargs.items():
-                if "__class__" not in key:
-                    setattr(self, key, val)
-
-    def __str__(self):
-        """ String representation """
-        return ('[{:s}] ({:s}) {}'.format(
-            self.__class__.__name__, self.id, self.__dict__))
+            models.storage.new(self)
 
     def save(self):
-        """ Updates the public instance attribute 'updated_at'. """
-        self.updated_at = datetime.now()
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """
-        Returns a dictionary with all keys/values of __dict__ of the instance.
+        """Return the dictionary of the BaseModel instance.
+
+        Includes the key/value pair __class__ representing
+        the class name of the object.
         """
         new_dict = self.__dict__.copy()
-        if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime(__time)
-        if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime(__time)
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
         new_dict["__class__"] = self.__class__.__name__
         return new_dict
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
