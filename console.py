8735b60169f68b97e5496a141dbbbaf82864b711
Bryan Somto <bryansomto@gmail.com>
2022-08-08 02:44:00 +0100

    extra modifications

--- console.py
+++ console.py
@@ -76,12 +76,12 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, arg):
         """Quit command to exit the program."""
-        exit()
+        return True
 
     def do_EOF(self, arg):
         """EOF signal to exit the program."""
         print("")
-        exit()
+        return True
 
     def do_create(self, arg):
         """Usage: create <class>
